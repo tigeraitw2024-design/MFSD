74e01adbc816d7f12043fc1c5ec0653d6540df1e
Tigerai_MKT <tiger.ai.tw2024@gmail.com>
2026-07-18 02:21:51 +0800

    許雅婷抬頭加回「工研院」

--- 專案/2_在職菁英課程/apps-script.js
+++ 專案/2_在職菁英課程/apps-script.js
@@ -170,7 +170,7 @@ function sendCourseConfirmEmail(data) {
     '──────────────────\n' +
     '本課程聯絡窗口\n' +
     '．虎智科技  紀先生   02-66058192\n' +
-    '．許雅婷   07-2625889 分機 117\n';
+    '．工研院  許雅婷  07-2625889 分機 117\n';
 
   MailApp.sendEmail({ to: to, subject: subject, body: body, name: '虎智科技 TigerAI' });
 }
